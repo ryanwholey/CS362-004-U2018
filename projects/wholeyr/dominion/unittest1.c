3160c8bf3051f68a0f9da8edd6f09b8c0563d8bc
Ryan Wholey <rjwholey@gmail.com>
2018-07-19 22:26:05 -0700

    redo unittest1, testing updateCoins instead

--- projects/wholeyr/dominion/unittest1.c
+++ projects/wholeyr/dominion/unittest1.c
@@ -16,20 +16,74 @@ void assertTrue(int assertion) {
   }
 }
 
-// TEST whoseTurn
+// TEST updateCoin
 
 int main () {
   // SETUP
-  printf("SUITE: whoseTurn\n");
-  int player = 27;
+  printf("SUITE: updateCoin\n");
+  int result;
+  int i;
+  int player = 0;
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+       remodel, smithy, village, baron, great_hall};
+
   struct gameState *state = newGame();
+  initializeGame(2, k, 3, state);
+
 
   // ASSERTIONS
-  printf("* Returns correct player when whoseTurn is set on the state: ");
-  state->whoseTurn = player;
-  int result = whoseTurn(state);
+  printf("* Should return 0 value: ");
+  result = updateCoins(player, state, 0);
+  assertTrue(result == 0);
 
-  assertTrue(result == player);
+  printf("* Should keep the same number of coins if the coin count is already correct: ");
+  updateCoins(player, state, 0);
+  assertTrue(state->coins == 3);
+
+  printf("* Should correct coins if coin count is off: ");
+  state->coins = 0;
+  updateCoins(player, state, 0);
+  assertTrue(state->coins == 3);
+
+  printf("* Should update coins if a new coin value is added: ");
+  state->coins = 0;
+  state->hand[player][0] = gold;
+  updateCoins(player, state, 0);
+  assertTrue(state->coins == 3 + 3);
+
+  for (i = 0; i < state->handCount[player]; i++) {
+    // set all cards to non treasures
+    state->hand[player][i] = estate;
+  }
+
+  printf("* Should work for gold pieces: ");
+  state->coins = 0;
+  state->hand[player][0] = gold;
+  updateCoins(player, state, 0);
+  assertTrue(state->coins == 3);
+  state->hand[player][0] = estate;
+
+  printf("* Should work for silver pieces: ");
+  state->coins = 0;
+  state->hand[player][0] = silver;
+  updateCoins(player, state, 0);
+  assertTrue(state->coins == 2);
+  state->hand[player][0] = estate;
+
+  printf("* Should work for copper pieces: ");
+  state->coins = 0;
+  state->hand[player][0] = copper;
+  updateCoins(player, state, 0);
+  assertTrue(state->coins == 1);
+  state->hand[player][0] = estate;
+
+  printf("* Should add bonus coins: ");
+  state->hand[player][0] = copper;
+  state->hand[player][1] = gold;
+  state->hand[player][2] = silver;
+  updateCoins(player, state, 5);
+  assertTrue(state->coins == 1 + 2 + 3 + 5);
+
 
   // CLEANUP
   free(state);
